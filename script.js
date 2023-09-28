8067215540a4de9a043b404b87701fec15792116
ytb <62708858+ytb664@users.noreply.github.com>
2023-09-28 15:19:44 +0700

    Add clear feature:

--- script.js
+++ script.js
@@ -1,7 +1,70 @@
 const display = document.querySelector('.display');
+const clear = document.querySelector('.clear');
+
 let displayedNumbers = [];
 
-displayNumbers();
+runCalculator();
+
+let firstNumber;
+let operator;
+let secondNumber;
+
+function runCalculator() {
+    displayNumbers();
+    setOperator();
+
+    clear.addEventListener('click', () => clearing());
+}
+
+function setFirstNumber() {
+
+    firstNumber = Number(displayedNumbers.join(''));
+    console.log(`firstNumber = ${firstNumber}`)
+
+    return firstNumber;
+}
+
+function setOperator() {
+
+    const clickedOperator = document.querySelectorAll('.operator .btn');
+
+    for (let el of clickedOperator) {
+        el.addEventListener('click', () => {
+
+            let theOperator = el.textContent;
+
+            if (firstNumber === undefined) {
+                setFirstNumber();
+                displayedNumbers = [];
+            } else if (secondNumber === undefined) {
+                secondNumber = setSecondNumber();
+                console.log(`secondNumber = ${secondNumber}`);
+
+                let result = operate(firstNumber, operator, secondNumber);
+                display.textContent = result;
+            }
+
+            operator = theOperator;
+
+            console.log(`operator : ${operator}`)
+        });
+    }
+}
+
+function setSecondNumber() {
+
+    secondNumber = displayedNumbers;
+}
+
+function clearing() {
+
+    firstNumber = undefined;
+    operator = undefined;
+    secondNumber = undefined;
+    displayedNumbers = [];
+
+    display.textContent = 0;
+}
 
 function displayNumbers () {
 
@@ -17,10 +80,6 @@ function displayNumbers () {
         });
     }
 }
-
-let firstNumber;
-let operator;
-let secondNumber;
 
 function add(num1, num2) {
     return num1 + num2;
